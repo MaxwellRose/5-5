72ec72f528570bad2b7e4d3796317c67630ae31e
Maxwell Rose <maxwell.rose@ocsbstudent.ca>
2021-03-18 13:51:14 +0000

    some errors

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 let number = 0
 
-
 document.getElementById('calculate').addEventListener
 ('click', addition)
 
 function addition () {
-number = document.getElementById('input1').value
+  number = document.getElementById('input1').value
 
-number = parseInt(number)
+  number = parseInt(number)
 
-number = number * number 
+  number = number * number
 
-alert(number)
+  alert(number)
 }
