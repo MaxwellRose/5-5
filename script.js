30003f9d4079b2f680ebf4e20728e63a950222c1
Maxwell Rose <maxwell.rose@ocsbstudent.ca>
2021-03-18 13:36:48 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+let number = 0
+
+
+document.getElementById('calculate').addEventListener
+('click', addition)
+
+function addition () {
+number = document.getElementById('input1').value
+
+number = parseInt(number)
+
+number = number * number 
+
+alert(number)
+}
